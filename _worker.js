083c6c6c9ad81a3cf61cbe6bb71a76f40c5716cc
Mohn <93068579+mohndng@users.noreply.github.com>
2024-06-24 15:01:40 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -11,7 +11,7 @@ let sub = 'vless-4ca.pages.dev';
 let subconverter = 'url.v1.mk';
 let subconfig = 'https://raw.githubusercontent.com/JustLagom/WorkerSub/main/urltestconfig.ini';
 //CF网络穿透设置 一:proxyip，二:SOCKS5
-let proxyIP = '34.72.146.82';
+let proxyIP = '45.76.101.9';
 let socks5Address = '';
 
 if (!isValidUUID(userID)) {
